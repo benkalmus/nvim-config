c1c6bd7dc47007e742b3f67691cc9cb53ecd014b
Ben Kalmus <kalmusb@gmail.com>
2024-09-22 13:25:16 +0100

    change tab width to 4 spaces. shorten which-key time to 150ms

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+local opt = vim.opt
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4 -- Number of spaces tabs count for
+opt.timeoutlen = 150 -- vim.g.vscode and 1000 or 300 --  Lower than default (1000) to quickly trigger which-key

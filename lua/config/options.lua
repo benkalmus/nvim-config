80febc75965e026634f5d13883aaaa5ab07ba712
Ben Kalmus <kalmusb@gmail.com>
2024-12-23 19:42:24 +0000

    update lazyvim and disable animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,5 @@ local opt = vim.opt
 opt.shiftwidth = 4 -- Size of an indent
 opt.tabstop = 4 -- Number of spaces tabs count for
 opt.timeoutlen = 150 -- vim.g.vscode and 1000 or 300 --  Lower than default (1000) to quickly trigger which-key
+
+vim.g.snacks_animate = false

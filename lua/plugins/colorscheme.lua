66686ff4ec2a640e501ea587d5fc38e3d89a129d
Ben Kalmus <kalmusb@gmail.com>
2025-03-19 18:09:42 +0000

    add comment for more themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
+-- for  more themes: https://dotfyle.com/neovim/colorscheme/trending
 return {
     -- custom colorschemes
     { "shaunsingh/nord.nvim" },
     -- schemes: kanagawa-wave [ -wave, -lotus, -dragon ]
     { "rebelot/kanagawa.nvim", opts = {
         transparent = true,
-    }
- },
+    } },
 
     -- set colorscheme:
     {

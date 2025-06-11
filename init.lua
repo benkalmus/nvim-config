c6ef4903a605280bbf16940859c90fd1b487da15
Ben Kalmus <ben.kalmus@algolia.com>
2025-06-11 13:27:32 +0100

    add python path to init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- virtual environment for python3 with pynvim installation
+vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim/venv/bin/python3")

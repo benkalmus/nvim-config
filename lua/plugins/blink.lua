d62059a1aa5f31b0e3b825fdbac4042376317bff
Ben Kalmus <ben.kalmus@algolia.com>
2025-06-12 15:12:37 +0100

    use blink instead of cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,24 +1,11 @@
 return {
     "saghen/blink.cmp",
+    -- enabled = true,
     opts = {
-        completion = {
-            list = {
-                selection = {
-                    preselect = true,
-                    auto_insert = false,
-                },
-            },
-        },
         keymap = {
             preset = "default",
-            ["<C-y>"] = { "select_and_accept", "fallback" },
-            ["<C-m>"] = { "select_and_accept", "fallback" },
-            ["<Tab>"] = { "select_and_accept", "fallback" },
-        },
-        -- experimental, signature help
-        signature = {
-            enabled = false,
-            window = { show_documentation = false },
+            ["<C-y>"] = { "select_and_accept" },
+            ["<Tab>"] = { "select_and_accept" },
         },
     },
 }
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,11 +1,11 @@
-return {}
--- local cmp = require("cmp")
--- return {
---     "hrsh7th/nvim-cmp",
---     opts = {
---         completion = {
---             completeopt = "menu,menuone,noinsert,popup",
---         },
---         preselect = cmp.PreselectMode.None,
---     },
--- }
+local cmp = require("cmp")
+return {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+    opts = {
+        completion = {
+            completeopt = "menu,menuone,noselect,noinsert,popup",
+        },
+        preselect = cmp.PreselectMode.None,
+    },
+}

6abb465ff98cc3f817737cc49db4c1feb0bbfdf6
Ben Kalmus <ben.kalmus@algolia.com>
2025-11-17 09:59:15 +0000

    update

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -43,7 +43,6 @@ return {
         -- or a list of adapter names,
         -- or a table of adapter names, mapped to adapter configs.
         -- The adapter will then be automatically loaded with the config.
-        adapters = {},
         -- Example for loading neotest-golang with a custom config
         adapters = {
             ["neotest-golang"] = {

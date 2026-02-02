e675b8a788007dd828534f067767fe545e98674a
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-02 12:32:54 +0000

    import lazyvim plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        textobjects = {
+            swap = {
+                enable = true,
+                -- swap_next = {
+                --     ["<leader>c]"] = "@parameter.inner",
+                -- },
+                -- swap_previous = {
+                --     ["<leader>c["] = "@parameter.inner",
+                -- },
+            },
+        },
+    },
+}

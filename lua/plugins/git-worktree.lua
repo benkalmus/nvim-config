df9710b6be4e19463666612541099a2acb2651a4
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-02 13:11:27 +0000

    fix diffview, flash, neotest and git-worktree

--- lua/plugins/git-worktree.lua
+++ lua/plugins/git-worktree.lua
@@ -1,3 +1,8 @@
 return {
     "ThePrimeagen/git-worktree.nvim",
+    keys = {
+        { "<leader>gwc", desc = "Create git worktree" },
+        { "<leader>gws", desc = "Switch to worktree" },
+        { "<leader>gwd", desc = "Delete git worktree" },
+    },
 }

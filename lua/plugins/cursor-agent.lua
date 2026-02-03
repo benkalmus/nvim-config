2d6213f9d221963885046deb1477d4c46e6d66fb
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-03 18:59:34 +0000

    use another cursor agent plugin with attached window

--- lua/plugins/cursor-agent.lua
+++ lua/plugins/cursor-agent.lua
@@ -1,5 +1,7 @@
 return {
-  "xTacobaco/cursor-agent.nvim",
+  -- "xTacobaco/cursor-agent.nvim", -- original creator
+  "waldnzwrld/cursor-agent.nvim",
+  branch = "sidebar-instead-of-floating-window",
   cmd = { "CursorAgent", "CursorAgentSelection", "CursorAgentBuffer" },
   keys = {
     { "<C-.>", "<cmd>CursorAgent<CR>", desc = "Cursor Agent: Toggle terminal", mode = { "n", "t" } },
@@ -14,6 +16,9 @@ return {
     multi_instance = false, -- Single instance mode
     timeout_ms = 60000,    -- 60 second timeout
     auto_scroll = true,    -- Auto-scroll terminal
+    window_mode = 'attached',
+    position = "right",
+    width = 0.4,
   },
   config = function(_, opts)
     require("cursor-agent").setup(opts)

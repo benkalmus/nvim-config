444fc9dd9cc31d5db45f41e26f37040eb5b53ff4
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-03 15:12:25 +0000

    fix harpoon and terminal mapping

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,11 @@
 return {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+    config = function()
+        require("harpoon"):setup()
+    end,
     keys = function()
         local keys = {
             {

16e21d7f315fbf161a2af6e1c7934e4727fbfdc7
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-03 20:05:29 +0000

    update settings for mkd preview

--- lua/plugins/live-preview.lua
+++ lua/plugins/live-preview.lua
@@ -1,9 +1,10 @@
 return {
     "brianhuster/live-preview.nvim",
+    event = 'VeryLazy',
     dependencies = {
         -- You can choose one of the following pickers
-        -- "ibhagwan/fzf-lua",
-        "nvim-telescope/telescope.nvim",
+        "ibhagwan/fzf-lua",
+        -- "nvim-telescope/telescope.nvim",
         -- "echasnovski/mini.pick",
         -- "folke/snacks.nvim",
     },

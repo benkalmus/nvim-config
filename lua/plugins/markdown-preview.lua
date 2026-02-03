afdf6ce4dc0aac6a85ad49e8f3fc465c8fb91cb6
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-03 21:29:51 +0000

    mkdp preview set fixed port and refresh on save

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -13,10 +13,12 @@ return {
             desc = "Markdown Preview",
         },
     },
-    config = function()
+    -- `init` runs before the plugin is loaded, `config` runs asfter.
+    init = function()
         vim.g.mkdp_auto_close = 0
-        -- vim.g.mkdp_port = '8080'
+        vim.g.mkdp_port = '8123'
         vim.g.mkdp_theme = 'dark'
+        vim.g.mkdp_refresh_slow = 1
 
         -- vim.cmd([[do FileType]])
     end,

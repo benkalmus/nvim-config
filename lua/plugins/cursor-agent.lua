f4ca868a35a3ded28ee9928f41cdfa4f87d9e466
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-09 19:52:28 +0000

    cursor selection keybind

--- lua/plugins/cursor-agent.lua
+++ lua/plugins/cursor-agent.lua
@@ -1,26 +1,26 @@
 return {
-  -- "xTacobaco/cursor-agent.nvim", -- original creator
-  "waldnzwrld/cursor-agent.nvim",
-  branch = "sidebar-instead-of-floating-window",
-  cmd = { "CursorAgent", "CursorAgentSelection", "CursorAgentBuffer" },
-  keys = {
-    { "<C-.>", "<cmd>CursorAgent<CR>", desc = "Cursor Agent: Toggle terminal", mode = { "n", "t" } },
-    { "<leader>Ca", "<cmd>CursorAgent<CR>", desc = "Cursor Agent: Toggle terminal", mode = "n" },
-    { "<leader>Ca", ":<C-u>CursorAgentSelection<CR>", desc = "Cursor Agent: Send selection", mode = "v" },
-    { "<leader>CA", "<cmd>CursorAgentBuffer<CR>", desc = "Cursor Agent: Send buffer", mode = "n" },
-  },
-  opts = {
-    cmd = "cursor-agent",  -- Cursor Agent CLI command
-    args = {},             -- Additional CLI arguments
-    use_stdin = true,      -- Send content via stdin
-    multi_instance = false, -- Single instance mode
-    timeout_ms = 60000,    -- 60 second timeout
-    auto_scroll = true,    -- Auto-scroll terminal
-    window_mode = 'attached',
-    position = "right",
-    width = 0.4,
-  },
-  config = function(_, opts)
-    require("cursor-agent").setup(opts)
-  end,
+	-- "xTacobaco/cursor-agent.nvim", -- original creator
+	"waldnzwrld/cursor-agent.nvim",
+	branch = "sidebar-instead-of-floating-window",
+	cmd = { "CursorAgent", "CursorAgentSelection", "CursorAgentBuffer" },
+	keys = {
+		{ "<C-.>", "<cmd>CursorAgent<CR>", desc = "Cursor Agent: Toggle terminal", mode = { "n", "t" } },
+		{ "<leader>Ca", "<cmd>CursorAgent<CR>", desc = "Cursor Agent: Toggle terminal", mode = "n" },
+		{ "<leader>CA", ":<C-u>CursorAgentSelection<CR>", desc = "Cursor Agent: Send selection", mode = "v" },
+		{ "<leader>CA", "<cmd>CursorAgentBuffer<CR>", desc = "Cursor Agent: Send buffer", mode = "n" },
+	},
+	opts = {
+		cmd = "cursor-agent", -- Cursor Agent CLI command
+		args = {}, -- Additional CLI arguments
+		use_stdin = true, -- Send content via stdin
+		multi_instance = false, -- Single instance mode
+		timeout_ms = 60000, -- 60 second timeout
+		auto_scroll = true, -- Auto-scroll terminal
+		window_mode = "attached",
+		position = "right",
+		width = 0.4,
+	},
+	config = function(_, opts)
+		require("cursor-agent").setup(opts)
+	end,
 }

52cc6230ddac7878732c99f889481b015f774f25
Ben Kalmus <ben.kalmus@algolia.com>
2026-02-12 17:27:18 +0000

    add ccc as color highlighter and picker

---
+++ lua/plugins/ccc.lua
@@ -0,0 +1,38 @@
+return {
+	"uga-rosa/ccc.nvim",
+	enabled = true, -- NvChad has built-in color highlighting
+	event = "VeryLazy",
+	config = function()
+		local ccc = require("ccc")
+		ccc.setup({
+			highlighter = {
+				auto_enable = true, -- Auto highlight colors in buffer
+				lsp = true, -- Use LSP for color detection
+			},
+			-- Available pickers
+			pickers = {
+				ccc.picker.hex, -- Hex color picker
+				ccc.picker.css_rgb, -- RGB picker
+				ccc.picker.css_hsl, -- HSL picker
+				ccc.picker.css_hwb, -- HWB picker
+				ccc.picker.css_lab, -- LAB picker
+				ccc.picker.css_lch, -- LCH picker
+				ccc.picker.css_oklab, -- OKLAB picker
+				ccc.picker.css_oklch, -- OKLCH picker
+			},
+			-- Output formats when inserting colors
+			outputs = {
+				ccc.output.hex, -- #RRGGBB
+				ccc.output.hex_short, -- #RGB (if possible)
+				ccc.output.css_rgb, -- rgb(R, G, B)
+				ccc.output.css_hsl, -- hsl(H, S%, L%)
+			},
+			alpha_show = "auto", -- Show alpha slider when color has alpha
+		})
+	end,
+	keys = {
+		{ "<leader>mc", "<cmd>CccPick<cr>", desc = "Color Picker" },
+		{ "<leader>mC", "<cmd>CccConvert<cr>", desc = "Color Convert" },
+		{ "<leader>mh", "<cmd>CccHighlighterToggle<cr>", desc = "Color Highlight Toggle" },
+	},
+}

96531204e8b2f4abe79daa49298e73a7d6872330
Ben Kalmus <ben.kalmus@algolia.com>
2026-04-01 13:31:49 +0100

    unpin and update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	-- branch = "main",
-	commit = "cc12e37e5bdc5467c9a06ab9b0887a97758f567f",
+	branch = "main",
+	-- commit = "cc12e37e5bdc5467c9a06ab9b0887a97758f567f",
 	opts = {
 		ensure_installed = {
 			"lua",

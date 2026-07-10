2ad1505f897e7649414e4efa9cd27155c09df262
Ben Kalmus <kalmusb@gmail.com>
2026-07-10 17:13:13 +0100

    fix clipboard on linux with wayland

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,6 +19,21 @@ opt.shiftwidth = 4
 opt.softtabstop = 4
 opt.expandtab = true
 
+-- Use wl-clipboard on Wayland (XWayland). xsel/xclip hang when clipboard
+-- owner is unresponsive; wl-copy/wl-paste don't block.
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy",
+    ["*"] = "wl-copy",
+  },
+  paste = {
+    ["+"] = "wl-paste",
+    ["*"] = "wl-paste",
+  },
+  cache_enabled = true,
+}
+
 -- Cache large-file check per buffer to avoid fs_stat syscall on every fold evaluation.
 local _fold_large_file_cache = {}
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {

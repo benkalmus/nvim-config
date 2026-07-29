ff51acb264924f78b8d2628080b398419f0a66a6
Ben Kalmus <ben.kalmus@algolia.com>
2026-07-29 17:40:35 +0100

    bufferline style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,7 +5,26 @@ return {
 		"nvim-mini/mini.tabline",
 		version = false,
 		config = function()
-			require("mini.tabline").setup({ show_icons = false })
+			require("mini.tabline").setup({
+				show_icons = true,
+				format = function(buf_id, label)
+					local modified = vim.bo[buf_id].modified and "+ " or ""
+					return MiniTabline.default_format(buf_id, label) .. modified .. "│"
+				end,
+			})
+
+			local function set_highlights()
+				local active = vim.api.nvim_get_hl(0, { name = "TabLineSel", link = false })
+				active.bold = true
+				vim.api.nvim_set_hl(0, "MiniTablineCurrent", active)
+				vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", active)
+			end
+
+			set_highlights()
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				group = vim.api.nvim_create_augroup("MiniTablineStyle", { clear = true }),
+				callback = set_highlights,
+			})
 		end,
 	},
 }
